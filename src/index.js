a46c0528d9ece20b7f849cba4cbfd9b103c20c83
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2020-10-29 16:48:32 +0200

    в процессе

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './styles.css';
+import debounce from 'lodash.debounce';
+
+const input = document.querySelector('input');
+const div = document.querySelector('.country');
+
+const searchCountry = function() {
+
+    fetch(`https://restcountries.eu/rest/v2/name/${input.value}`)
+    .then(data => data.json())
+    .then(data => {
+        if (data.length = 1) {
+            div.innerHTML += `<h1>${data[0].name}</h1>`;
+        }
+    })
+
+
+
+}
+
+input.addEventListener('input', debounce(searchCountry, 500));
+
