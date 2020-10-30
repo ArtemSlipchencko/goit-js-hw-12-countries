3df8b72affdc0aaaaad00b23fab93f83939e2780
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2020-10-30 14:07:23 +0200

    pnotify

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ const searchCountry = function() {
 
     fetch(`https://restcountries.eu/rest/v2/name/${input.value}`)
     .then(data => data.json())
+    .then(data => console.log(data))
     .then(data => {
         if (data.length = 1) {
             div.innerHTML += `<h1>${data[0].name}</h1>`;
         }
     })
+    
 
 
 
@@ -20,3 +22,4 @@ const searchCountry = function() {
 
 input.addEventListener('input', debounce(searchCountry, 500));
 
+alert('Notice me, senpai!');
